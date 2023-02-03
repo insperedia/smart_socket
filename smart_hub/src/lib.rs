d8dc328a705e39ca45486def713db5800af71a6f
verifiera <oleg@verifiera.se>
2023-02-03 10:12:42 +0100

    Final

--- smart_hub/src/lib.rs
+++ smart_hub/src/lib.rs
@@ -6,13 +6,3 @@ pub mod client;
 pub mod errors;
 pub mod devices;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
